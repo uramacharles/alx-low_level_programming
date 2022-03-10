9a62b7b0c3a5046f1f9cc432c1d29d5cf0140b21
uramacharles <chiemekacharles@gmail.com>
2022-03-10 07:25:38 -0800

    noe

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* This is a printf test file
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
