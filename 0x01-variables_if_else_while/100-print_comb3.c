03ad8ade673dd9a536ba6af4afaaf9b0b12b1eb3
uramacharles <chiemekacharles@gmail.com>
2022-03-11 12:39:34 -0800

    now

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point. this Turns a character into a small letter
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = '0'; x < '9'; x++)
+	{
+		for (y = x + 1; y <= '9'; y++)
+		{
+			putchar(x);
+			putchar(y);
+			if ( x == '8' && y == '9')
+			{
+				continue;
+			}
+			else if (x != '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			else
+			{
+				continue;
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
