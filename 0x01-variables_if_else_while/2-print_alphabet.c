3e7bc13741563587674b18f08afececea2f2d851
uramacharles <chiemekacharles@gmail.com>
2022-03-11 07:17:09 -0800

    now

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	int x;
 
-	for (x = 'a'; x<='z'; x++)
+	for (x = 'a'; x <= 'z'; x++)
 	{
 		putchar(x);
 	}
 	putchar('\n');
-    return (0);
+	return (0);
 }
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -19,5 +19,5 @@ int main(void)
 		putchar(x);
 	}
 	putchar('\n');
-    return (0);
+	return (0);
 }
