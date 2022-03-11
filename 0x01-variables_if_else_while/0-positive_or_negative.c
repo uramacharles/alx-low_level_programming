4c757ba1bf6f636839eef60507bd55dca0c3d590
uramacharles <chiemekacharles@gmail.com>
2022-03-11 04:45:08 -0800

    now

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-/*
-* Main - This file is for 0-positive_or_negative.c
+/**
+* Main - Entry point
+* This file is for 0-positive_or_negative.c
 *
 * Return: Always 0 (Sucess)
 */
