2c604c2d8f7e7d51040175ee2e0c2463803a9ad1
uramacharles <chiemekacharles@gmail.com>
2022-03-11 20:01:31 -0800

    now

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,41 +8,41 @@
 */
 int main(void)
 {
-	int x;
-	int y;
-	int z;
-	int p;
-	int c1;
-	int c2;
+int x;
+int y;
+int z;
+int p;
+int c1;
+int c2;
 
-	for (x = '0'; x <= '9'; x++)
+for (x = '0'; x <= '9'; x++)
+{
+	for (y = '0'; y <= '9'; y++)
 	{
-		for (y = '0'; y <= '9'; y++)
+		p = y + 1;
+		for (z = '0'; z <= '9'; z++)
 		{
-			p = y + 1;
-			for (z = '0'; z <= '9'; z++)
+			for (; p <= '9'; p++)
 			{
-				for (; p <= '9'; p++)
+				c1 = (x * 10) + y;
+				c2 = (z * 10) + p;
+				if (c1 < c2)
 				{
-					c1 = (x * 10) + y;
-					c2 = (z * 10) + p;
-					if (c1 < c2)
-					{
-						putchar(x);
-						putchar(y);
-						putchar(' ');
-						putchar(z);
-						putchar(p);
-						if (x == '9' && y == '8' && z == '9' && p == '9')
-							continue;
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(x);
+					putchar(y);
+					putchar(' ');
+					putchar(z);
+					putchar(p);
+					if (x == '9' && y == '8' && z == '9' && p == '9')
+						continue;
+					putchar(',');
+					putchar(' ');
 				}
-				p = '0';
 			}
+			p = '0';
 		}
 	}
-	putchar('\n');
-	return (0);
+}
+putchar('\n');
+return (0);
 }
