78f2febffe4f6d0e1a366db8e6b8603b40d7e2e6
uramacharles <chiemekacharles@gmail.com>
2022-03-11 19:49:10 -0800

    now

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,46 +8,37 @@
 */
 int main(void)
 {
-	int i;
-	int e;
-	int g;
-	int h;
-	int op1;
-	int op2;
+	int x;
+	int y;
+	int z;
+	int p;
 
-	i = e = g = h = 48;
-	while (h < 58)
+	for (x = '0'; x <= '9'; x++)
 	{
-		g = 48;
-		while (g < 58)
+		for (y = '0'; y < '9'; y++)
 		{
-			e = 48;
-			while (e < 58)
+			for (z = '0'; z <= '9'; z++)
 			{
-				i = 48;
-				while (i < 58)
+				for (p = '0'; p <= '9'; p++)
 				{
-					op1 = (h * 10) + g;
-					op2 = (e * 10) + i;
-					if (op1 < op2)
+					c1 = (x * 10) + y;
+					c2 = (z * 10) + p;
+					if (c1 < c2)
 					{
-						putchar(h);
-						putchar(g);
+						putchar(x);
+						putchar(y);
 						putchar(' ');
-						putchar(e);
-						putchar(i);
-						if (h == 57 && g == 56 && e == 57 && i == 57)
-							break;
+						putchar(z);
+						putchar(p);
+						if (x == '9' && y == '8' && z == '9' && p == '9')
+							continue;
 						putchar(',');
 						putchar(' ');
 					}
-					i++;
 				}
-				e++;
+				p = '0';
 			}
-			g++;
 		}
-		h++;
 	}
 	putchar('\n');
 	return (0);
