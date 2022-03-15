de3b3202d51e8425bccd2d589cec979aa6087390
uramacharles <chiemekacharles@gmail.com>
2022-03-15 07:36:59 -0700

    now

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 int _putchar(char);
@@ -14,3 +14,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int);
 void print_times_table(int);
+
+#endif
