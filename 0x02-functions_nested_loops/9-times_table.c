7692e6c0e808f9f65c532973189b35e848a48341
uramacharles <chiemekacharles@gmail.com>
2022-03-15 13:30:15 -0700

    now

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,27 +15,25 @@ void times_table(void)
 	for (i = 0; i < 10; i++)
 	{
 		c = 0;
-		for (j = 0; j < 10; j++)
+		for (j = 0; j < 9; j++)
 		{
 			c += i;
 			if (j == 0)
 			{
 				_putchar('0' + 0);
-				_putchar(',');
-				_putchar(' ');
 			}
 			if (c < 10)
 			{
-				_putchar('0' + c);
 				_putchar(',');
 				_putchar(' ');
+				_putchar('0' + c);
 			}
 			if (c >= 10)
 			{
-				_putchar('0' + (c / 10));
-				_putchar('0' + (c % 10));
 				_putchar(',');
 				_putchar(' ');
+				_putchar('0' + (c / 10));
+				_putchar('0' + (c % 10));
 			}
 		}
 		_putchar('\n');
