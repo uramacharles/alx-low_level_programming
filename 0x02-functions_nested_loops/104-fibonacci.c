c3139c3bf0f0231dd16fa24ce044421096799307
uramacharles <chiemekacharles@gmail.com>
2022-03-15 20:03:19 -0700

    now

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	unsigned long int a, b, c, b1, b2, b3, b4;
+
+	b = 1;
+	c = 2;
+	printf("%lu", b);
+
+	for (a = 1, a < 91; a++)
+	{
+		printf(", %lu", c);
+		c = c + b;
+		b = c - b
+	}
+	b1 = b / 1000000000;
+	b2 = b % 1000000000;
+	b3 = c / 1000000000;
+	b4 = c % 1000000000;
+
+	for (a = 92; a < 99; ++a)
+	{
+		printf(", %lu", b3 + (b4 / 1000000000));
+		printf("%lu", b4 % 1000000000);
+		b3 += b1;
+		b1 = b3 - b1;
+		b4 += b2;
+		b2 = b4 - b2;
+	}
+	printf("\n");
+	return (0);
+}
