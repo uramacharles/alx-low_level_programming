f4ea4137fe9e217db83c8aadbfaaabb7a8f3ab5d
uramacharles <chiemekacharles@gmail.com>
2022-03-17 12:51:02 -0700

    now

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* @x: the input
+* Return: Always 0 (Success)
+*/
+void print_numbers(void)
+{
+	int i;
+	
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0', + i);
+	}
+	_putchar('\n');
+}
