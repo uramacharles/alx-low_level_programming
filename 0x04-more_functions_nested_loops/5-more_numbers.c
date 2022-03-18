a3d1bc29d009ac9b8e05627163a47d74d8146582
uramacharles <chiemekacharles@gmail.com>
2022-03-17 17:35:15 -0700

    now

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,12 +13,8 @@ void more_numbers(void)
 		while (j <=14)
 		{
 			if (j > 9)
-			{
 				_putchar('0' + j / 10);
-				_putchar('0' + j % 10);
-			}
-			else
-				_putchar('0' + j);
+			_putchar('0' + j % 10);
 			j++;
 		}
 		_putchar('\n');
