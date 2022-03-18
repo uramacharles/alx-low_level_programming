ba4cabf89a9e801fe62829938178d8458e9566c2
uramacharles <chiemekacharles@gmail.com>
2022-03-17 17:14:54 -0700

    all

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,13 @@
 
 /**
 * main - Entry point
-* @x: the input
+* the input
 * Return: Always 0 (Success)
 */
 void print_numbers(void)
 {
 	int i;
-	
+
 	for (i = 0; i <= 9; i++)
 	{
 		_putchar('0', + i);
