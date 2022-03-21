24d4985a1b5aa3889c9fe8cbea5bf69c31f1fdab
uramacharles <chiemekacharles@gmail.com>
2022-03-21 14:33:32 -0700

    now

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,15 +3,15 @@
 
 int _putchar(char);
 void reset_to_98(int *x);
-void swap_int(int, int);
-int _strlen(char);
-void _puts(char);
-void print_rev(char);
-void rev_string(char);
-void puts2(char);
-void puts_half(char);
-void print_array(int, int);
-char *_strcpy(char, char);
+void swap_int(int *x, int *y);
+int _strlen(char *x);
+void _puts(char *x);
+void print_rev(char *x);
+void rev_string(char *x);
+void puts2(char *x);
+void puts_half(char *x);
+void print_array(int *x, int y);
+char *_strcpy(char *dest, char *src);
 int _atoi(char);
 
 #endif
