179dd7a429e0fc9bf250b3ad26773e72371667c4
uramacharles <chiemekacharles@gmail.com>
2022-04-08 04:38:04 -0700

    now

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n > counts2)
 		n = counts2;
 
-	countoutpt = counts2 + n;
+	countoutpt = counts1 + n;
 
 	outpt = malloc(countoutpt + 1);
 
