97467c93e26699c71120801f81290bb2d2acacac
uramacharles <chiemekacharles@gmail.com>
2022-05-06 04:49:50 -0700

    now

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,18 +11,20 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int file, count, writer;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
 
 	file = open(filename, O_WRONLY | O_APPEND);
 	if (!file)
 		return (-1);
-	for (count = 0; text_content[count]; count++)
-		;
-	writer = write(file, text_content, count);
-	if (writer == -1)
-		return (-1);
-
+	if (text_content)
+	{
+		for (count = 0; text_content[count]; count++)
+			;
+		writer = write(file, text_content, count);
+		if (writer == -1)
+			return (-1);
+	}
 	close(file);
 	return (1);
 }
